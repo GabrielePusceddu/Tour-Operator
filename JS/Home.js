44515bfd00030bb8e6ea4db85ab4ed62f8ddd729
GabrielePusceddu <gabrielepusceddu21@gmail.com>
2024-07-11 15:32:37 +0200

    mod5

--- JS/Home.js
+++ JS/Home.js
@@ -3,3 +3,28 @@ document.querySelectorAll('.form-check-input').forEach(function(element) {
       event.stopPropagation();
     });
   });
+
+  const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
